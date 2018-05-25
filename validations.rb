0278d503b3bda807654460ead5cd4a1472355545
HIRO <takatahirofumi@HIRO-MacBook-Air.local>
2018-05-25 21:35:18 +0900

    create validate_1

--- validations.rb
+++ validations.rb
@@ -1,2 +1,14 @@
 module Validations
+
+  def validate_1
+    if @fs == 1 && @b[@first_p].include?("^")
+      puts @error_2
+    elsif @fs == 2 && @b[@first_p] =~ /^\p{Han}|^\p{Hiragana}/
+      puts @error_2
+    elsif @b[@first_p] == "\s\s\s"
+      puts @error_2
+    else
+    end
+  end
+
 end
